88111621c202cf04ffb1b434789f1e4b648c0dc9
agent-indigo <bradenhynes@gmail.com>
2026-07-20 16:41:59 -0230

    Fix: response JSON not appearing in Postman

--- src/main/java/com/keyin/hynes/braden/quarkusql/controllers/ListingRestController.java
+++ src/main/java/com/keyin/hynes/braden/quarkusql/controllers/ListingRestController.java
@@ -1,17 +1,16 @@
 package com.keyin.hynes.braden.quarkusql.controllers;
-import java.util.List;
-import com.keyin.hynes.braden.quarkusql.entities.Listing;
 import com.keyin.hynes.braden.quarkusql.repositories.ListingRepository;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 @Path("/listings")
 public final class ListingRestController {
   @Inject
   private final ListingRepository listingRestRepository = new ListingRepository();
   @GET
   @Path("/")
-  public List<Listing> getAll() {
-    return listingRestRepository.findAll().list();
+  public Response getAll() {
+    return Response.ok(listingRestRepository.findAll().list()).build();
   }
 }
--- src/main/java/com/keyin/hynes/braden/quarkusql/controllers/RealtorRestController.java
+++ src/main/java/com/keyin/hynes/braden/quarkusql/controllers/RealtorRestController.java
@@ -1,17 +1,16 @@
 package com.keyin.hynes.braden.quarkusql.controllers;
-import java.util.List;
-import com.keyin.hynes.braden.quarkusql.entities.Realtor;
 import com.keyin.hynes.braden.quarkusql.repositories.RealtorRepository;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 @Path("/realtors")
 public final class RealtorRestController {
   @Inject
   private final RealtorRepository realtorRepository = new RealtorRepository();
   @GET
   @Path("/")
-  public List<Realtor> getAll() {
-    return realtorRepository.findAll().list();
+  public Response getAll() {
+    return Response.ok(realtorRepository.findAll().list()).build();
   }
 }
